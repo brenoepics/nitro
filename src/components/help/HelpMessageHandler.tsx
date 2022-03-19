976bbe1bc9f70f8e9f22057a9c1ea7841732e673
dank074 <torresefrain10@gmail.com>
2022-03-19 03:09:21 -0500

    add missing cfh packet listeners

--- src/components/help/HelpMessageHandler.tsx
+++ src/components/help/HelpMessageHandler.tsx
@@ -1,4 +1,4 @@
-import { CallForHelpResultMessageEvent, GetPendingCallsForHelpMessageComposer, IssueCloseNotificationMessageEvent } from '@nitrots/nitro-renderer';
+import { CallForHelpDisabledNotifyMessageEvent, CallForHelpPendingCallsDeletedMessageEvent, CallForHelpPendingCallsMessageEvent, CallForHelpReplyMessageEvent, CallForHelpResultMessageEvent, DeletePendingCallsForHelpMessageComposer, GetPendingCallsForHelpMessageComposer, IssueCloseNotificationMessageEvent } from '@nitrots/nitro-renderer';
 import { FC, useCallback } from 'react';
 import { LocalizeText, NotificationAlertType, NotificationUtilities, SendMessageComposer } from '../../api';
 import { UseMessageEventHook } from '../../hooks';
@@ -43,6 +43,49 @@ export const HelpMessageHandler: FC<{}> = props =>
     }, []);
 
     UseMessageEventHook(IssueCloseNotificationMessageEvent, onIssueCloseNotificationMessageEvent);
+
+    const onCallForHelpPendingCallsMessageEvent = useCallback((event: CallForHelpPendingCallsMessageEvent) =>
+    {
+        const parser = event.getParser();
+
+        if(parser.count > 0)
+        {
+            NotificationUtilities.confirm(LocalizeText('help.emergency.pending.title') + '\n' + parser.pendingCalls[0].message, () =>
+            {
+                SendMessageComposer(new DeletePendingCallsForHelpMessageComposer());
+            }, null, LocalizeText('help.emergency.pending.button.discard'), LocalizeText('help.emergency.pending.button.keep'), LocalizeText('help.emergency.pending.message.subtitle'));
+        }
+        
+    }, []);
+
+    UseMessageEventHook(CallForHelpPendingCallsMessageEvent, onCallForHelpPendingCallsMessageEvent);
+
+    const onCallForHelpPendingCallsDeletedMessageEvent = useCallback((event: CallForHelpPendingCallsDeletedMessageEvent) =>
+    {
+        const message = 'Your pending calls were deleted'; // todo: add localization
+
+        NotificationUtilities.simpleAlert(message, NotificationAlertType.MODERATION, null, null, LocalizeText('mod.alert.title'));
+    }, []);
+
+    UseMessageEventHook(CallForHelpPendingCallsDeletedMessageEvent, onCallForHelpPendingCallsDeletedMessageEvent);
+
+    const onCallForHelpReplyMessageEvent = useCallback((event: CallForHelpReplyMessageEvent) =>
+    {
+        const parser = event.getParser();
+
+        NotificationUtilities.simpleAlert(parser.message, NotificationAlertType.MODERATION, null, null, LocalizeText('help.cfh.reply.title'));
+    }, []);
+    
+    UseMessageEventHook(CallForHelpReplyMessageEvent, onCallForHelpReplyMessageEvent);
+
+    const onCallForHelpDisabledNotifyMessageEvent = useCallback((event: CallForHelpDisabledNotifyMessageEvent) =>
+    {
+        const parser = event.getParser();
+
+        NotificationUtilities.simpleAlert(LocalizeText('help.emergency.global_mute.message'), NotificationAlertType.MODERATION, parser.infoUrl, LocalizeText('help.emergency.global_mute.link'), LocalizeText('help.emergency.global_mute.subtitle'))
+    }, []);
+
+    UseMessageEventHook(CallForHelpDisabledNotifyMessageEvent, onCallForHelpDisabledNotifyMessageEvent);
 
     return null;
 }
